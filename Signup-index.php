791ced0bc167f5b5f8067fb7c156c7006e97fae3
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2024-05-22 16:32:30 +0800

    Fixed Sign Up Index

--- Signup-index.php
+++ Signup-index.php
@@ -1,34 +1,29 @@
 <?php
-
+require('./backend/session.php');
+require('./backend/database.php');
 $email = $_POST['email'];
 $password = $_POST['password'];
 $name = $_POST['name'];
 $phoneNumber = $_POST['phoneNumber'];
 $address = $_POST['address'];
 
-$servername = "localhost";
-$username = "root";
-$password = "";  // Replace with your actual MySQL root password
-$dbname = "digi_dreamers";       // Replace with your database name
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 
-// Perform a query, example: insert data
-$sql = "INSERT INTO users (email, password, name, phoneNumber, address) VALUES ('john@example.com', 'securepassword', 'john_doe', '09451300880', '8692 Fortuna.');
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+
+$query = "INSERT INTO user_credentials (id, username, password, name, phonenumber, address) VALUES (null, '$email', md5('$password'), '$name', '$phoneNumber', '$address')";
+if (mysqli_query($conn, $query)) {
+    $_SESSION['session'] = 'valid';
+    echo "<script>window.location.href = '/jedgeesmain/Homepage-Index.php'</script>";
+
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close connection
+
 $conn->close();
 
 ?>
