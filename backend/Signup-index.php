3e7b1c0e31954bb49e2b886737995706ac5ffb8d
jdyyy1 <159746954+jdyyy1@users.noreply.github.com>
2024-06-07 17:52:05 +0800

    Update Signup-index.php

--- backend/Signup-index.php
+++ backend/Signup-index.php
@@ -3,9 +3,15 @@ require('./session.php');
 require('./database.php');
 $email = $_POST['email'];
 $password = $_POST['password'];
-$name = $_POST['name'];
-$phoneNumber = $_POST['phoneNumber'];
-$address = $_POST['address'];
+$fname = $_POST['fname'];
+$mname = $_POST['mname'];
+$lname = $_POST['lname'];
+$contactnum = $_POST['contactnum'];
+$streetNumber = $_POST['streetNumber'];
+$streetName = $_POST['streetName'];
+$barangay = $_POST['barangay'];
+$city = $_POST['city'];
+$postal = $_POST['postal'];
 
 
 if ($conn->connect_error) {
@@ -14,7 +20,7 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 
-$query = "INSERT INTO user_credentials (id, username, password, name, phonenumber, address) VALUES (null, '$email', md5('$password'), '$name', '$phoneNumber', '$address')";
+$query = "INSERT INTO user_credentials (id, username, password, fname, mname, lname, contactnum, streetNumber, streetName, barangay, city, postal) VALUES (null, '$email', md5('$password'), '$fname', '$mname', '$lname', '$streetNumber', '$streetName', '$barangay', '$city', '$postal')";
 if (mysqli_query($conn, $query)) {
     $_SESSION['session'] = 'valid';
     echo "<script>window.location.href = '/jedgeesmain/Homepage-Index.php'</script>";
@@ -22,7 +28,6 @@ if (mysqli_query($conn, $query)) {
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 
 $conn->close();
 
