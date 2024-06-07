a0e19aade68336c548f122a4e3fc7a6cdbd61eb9
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2024-06-08 07:25:37 +0800

    changes

--- scripts/customize-tshirt.js
+++ scripts/customize-tshirt.js
@@ -1,63 +1,65 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkprice = document.getElementById('checkprice_shirt');
+    var checkprice = document.getElementById('checkprice_shirt'); // Corrected
     var submit_shirt = document.getElementById('submit-shirt');
     var form_shirt = document.getElementById('form-shirt');
     var resetButton = document.getElementById('reset-shirt');
     var resibo = document.getElementById('resibo-shirt');
 
-    var shirt_size = document.getElementsByClassName('shirt_size').value;
-    var shirt_num = document.getElementsById('numShirt').value;
-    var print_size = document.getElementsByClassName('print-size_shirt').value;
-    var material_type = document.getElementsByClassName('shirt_material').value;
-    var vinyl_type = document.getElementsByClassName('typevinyl').value;
-   
-    var shirtprice = 0;
-    var shirt_nummultiply = shirt_num;
-    var print_sizeprice = 0;
-    var vinyl_typeprice = 0;
+    
 
-    var totalprice = (shirtprice + print_sizeprice + vinyl_typeprice) * shirt_nummultiply;
-
+    
 
     function compute(){
+        var shirt_size = document.getElementById('shirt_size').value;
+        var print_size = document.getElementById('print-size_shirt').value;
+        var material_type = document.getElementById('shirt_material').value;
+        var vinyl_type = document.getElementById('typevinyl').value;    
+        var shirt_num_string = document.getElementById('numShirt').value;
+        var shirt_num = parseFloat(shirt_num_string);
+    
+        var shirtprice = 0;
+        var print_sizeprice = 0;
+        var vinyl_typeprice = 0;
+    
+
         switch (shirt_size) {
             case "XS": 
-                if (material_type == "softcotton"){
+                if (material_type == "Soft Cotton"){
                     shirtprice += 120;
                 }
-                else if (material_type == "premcotton"){
+                else if (material_type == "Prem Cotton"){
                     shirtprice += 150;
                 }
             break;
             case "S": 
-                if (material_type == "softcotton"){
+                if (material_type == "Soft Cotton"){
                     shirtprice += 120;
                 }
-                else if (material_type == "premcotton"){
+                else if (material_type == "Prem Cotton"){
                     shirtprice += 150;
                 }
             break;
             case "M": 
-                if (material_type == "softcotton"){
+                if (material_type == "Soft Cotton"){
                     shirtprice += 120;
                 }
-                else if (material_type == "premcotton"){
+                else if (material_type == "Prem Cotton"){
                     shirtprice += 150;
                 }
             break;
             case "L": 
-                if (material_type == "softcotton"){
+                if (material_type == "Soft Cotton"){
                     shirtprice += 135;
                 }
-                else if (material_type == "premcotton"){
+                else if (material_type == "Prem Cotton"){
                     shirtprice += 165;
                 }
             break;
             case "XL": 
-                if (material_type == "softcotton"){
+                if (material_type == "Soft Cotton"){
                     shirtprice += 135;
                 }
-                else if (material_type == "premcotton"){
+                else if (material_type == "Prem Cotton"){
                     shirtprice += 165;
                 }
             break;
@@ -66,23 +68,23 @@ document.addEventListener('DOMContentLoaded', function() {
             break;
         }
         switch(vinyl_type){
-            case "holo":
+            case "Holo":
                 vinyl_typeprice += 10;
             break;
-            case "metal":
+            case "Metal":
                 vinyl_typeprice += 10;
             break;
-            case "glitter":
+            case "Glitter":
                 vinyl_typeprice += 15;
             break;
-            case "glow":
+            case "Glow":
                 vinyl_typeprice += 25;
             break;
-            case "gamuza":
+            case "Gamuza":
                 vinyl_typeprice += 30;
             break;
             default:
-                console.log("error in computing");
+                console.log("error in computing2");
             break;
         }
         switch(print_size){
@@ -120,10 +122,11 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             break;
             default:
-                console.log("error in computing");
+                console.log("error in computing3");
             break;
         }
 
+        var totalprice = (shirtprice + print_sizeprice + vinyl_typeprice) * shirt_num;
 
         
         document.getElementById('size-shirt').textContent = shirt_size;
@@ -131,6 +134,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('material-shirt').textContent = material_type;
         document.getElementById('Printvinyl').textContent = vinyl_type;
         document.getElementById('num-shirt').textContent = shirt_num;
+
         
         
 
@@ -141,7 +145,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('shirt_price').value = shirtprice.toFixed(2);
         document.getElementById('vinyl_price').value = vinyl_typeprice.toFixed(2);
         document.getElementById('print_sizeprice').value = print_sizeprice.toFixed(2);
-        document.getElementById('total-price').value = totalprice.toFixed(2);
+        document.getElementById('total-price-shirt').value = totalprice.toFixed(2);
 
     }
 
